e053947e7cec7de912da2066f23fd6e90a333cec
Scott Kirkpatrick <scottkirk@scottkirk.dev>
2022-05-24 21:43:39 -0400

    Add back rust wasm build

--- services/webserver/hostHugo.sh
+++ services/webserver/hostHugo.sh
@@ -1,5 +1,5 @@
-#sudo docker run -it -v ~/repos/scottkirk.dev/game:/usr/src/game rust-wasm:latest \
-#	/bin/sh -c "cd /usr/src/game && wasm-pack build --target web"
+sudo docker run -it -v ~/repos/scottkirk.dev/game:/usr/src/game rust-wasm:latest \
+  /bin/sh -c "cd /usr/src/game && wasm-pack build --target web"
 sudo docker run -it -v ~/repos/scottkirk.dev:/src jakejarvis/hugo-extended:latest
 sudo chown scott:scott -R ~/repos/scottkirk.dev
 rm -rf ~/services/webserver/data/public/*
